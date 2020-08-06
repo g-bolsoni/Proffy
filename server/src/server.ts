43a1e19f47b5d82fc02c9bf72d018536c9c1e42f
g-bolsoni <giovanebolsoni2@gmail.com>
2020-08-06 08:51:15 -0300

    BD is ready

--- server/src/server.ts
+++ server/src/server.ts
@@ -8,8 +8,7 @@ app.use(express.json())
 //https://localhost:3333/contacts
 app.get('/users',(req,res) =>{
     const {name, age} = req.body
-    console.log('56:17')
-    console.log(`My name is ${name} and i have ${age} years old 56:17`)
+    console.log(`My name is ${name} and i have ${age} years old `)
     return res.json(name)
 });
 
